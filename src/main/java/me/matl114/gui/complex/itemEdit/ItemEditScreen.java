b2b9fc2845694abdbf92ef54538df88715e20af1
rennaikankk-cmyk <rennaikankk@gmail.com>
2026-09-12 12:29:20 +0800

    版本号 1.2.3.0: 鞘翅追人全面升级+开关UI重做+移除Freecam
    - 鞘翅追人(v1/v2全模式): 拦截制导/扇形避障/近身收敛/丢失目标记忆
      SPEAR: 主动进攻+波浪绕圈攒矛充能+威胁判定朝向检查+闪避冷却
      MACE: 接近段水平拦截(高度归状态机), 俯冲段保持直线攒速
    - 布尔开关重做: 滑块开关(绿=开/透明=关), 替换原版按钮贴图
    - 移除Freecam(灵魂出窍, 与其他客户端冲突)

--- src/main/java/me/matl114/gui/complex/itemEdit/ItemEditScreen.java
+++ src/main/java/me/matl114/gui/complex/itemEdit/ItemEditScreen.java
@@ -20,6 +20,7 @@ import me.matl114.gui.basic.*;
 import me.matl114.gui.complex.config.KeyValueInputWidget;
 import me.matl114.gui.complex.config.ListModifyWidget;
 import me.matl114.gui.elements.ButtonElement;
+import me.matl114.gui.elements.ToggleSwitchElement;
 import me.matl114.gui.elements.IconElement;
 import me.matl114.gui.elements.LabelElement;
 import me.matl114.gui.elements.SlotElement;
@@ -503,11 +504,9 @@ public class ItemEditScreen extends ConfirmingBigScreen {
                         var sec = flags[index];
                         subScreenWidget.addDrawableChild(ExecutableWidget.instance(
                                         50 + 1 + 20 * index, 1, 20 - 2, 20 - 2)
-                                .setElementHandler(IconElement.statedGuiPredicate(
-                                                ButtonElement.BUTTON,
-                                                ButtonElement.BUTTON_INACTIVE,
+                                .setElementHandler(new ToggleSwitchElement(
                                                 ButtonAction.run(() -> sec.setHideFlag(sample, !sec.isHide(sample))),
-                                                (bl) -> sec.isHide(sample))
+                                                () -> sec.isHide(sample))
                                         .withTooltips(TooltipHandler.of(List.of(Text.literal(sec.displayName()))))));
                     }
                     return subScreenWidget;
